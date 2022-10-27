c91014b9d5dd4bb67333ecd4d9f99469ede7b27e
Igor Skipovskiy <36971976+meAsakura@users.noreply.github.com>
2022-10-27 15:07:50 +0300

    Styles update
    Added styles to main pages
    Added datatable bundle

--- resources/views/control.blade.php
+++ resources/views/control.blade.php
@@ -5,34 +5,22 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Notebooks</title>
-        <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    </head>
-    <body class="">
+<link href="{{ asset('styles.css') }}" rel="stylesheet">  
+</head>
+<body>
+        <div class="container">
         <h1>Notebook's control</h1>
         <div>
             @if($errors->any())
                 <h4>{{$errors->first()}}</h4>
             @endif
-        </div>
         <div>
-            <form method="GET" action="{{ route('notebooks') }}">
-                @csrf
-                <div class="row">
-                    <div>
-                        <button type="submit" class="btn btn-primary">
-                            {{ __('Show notebooks') }}
-                        </button>
-                    </div>
-                </div>
-            </form>
-            <div class="row">
-                <label for="number" class="">{{ __('Notebook ID to show') }}</label>
-            </div>
             <form method="POST" action="{{ route('notebook') }}">
                 @csrf
-                <div class="col-md-1">
-                    <input id="number" type="search" class="form-control @error('number') is-invalid @enderror" name="number" required autocomplete="current-number">
+                <div class="m-2">
+                    <label for="number" class="">{{ __('Notebook ID to show') }}</label>
+                    <input id="number" type="search" class="form-control" @error('number') is-invalid @enderror name="number" required autocomplete="current-number">
 
                     @error('number')
                         <span class="invalid-feedback" role="alert">
@@ -48,9 +36,9 @@
         </form>
         <form method="DELETE" action="{{ route('delete_by_id') }}">
             @csrf
-            <div class="col-md-1">
+            <div class="m-2">
                 <label for="number" class="">{{ __('Notebook ID to delete') }}</label>
-                <input id="number" type="search" class="form-control @error('number') is-invalid @enderror" name="number" required autocomplete="current-number">
+                <input id="number" type="search" class="form-control"  @error('number') is-invalid @enderror name="number" required autocomplete="current-number">
 
                 @error('number')
                     <span class="invalid-feedback" role="alert">
@@ -62,7 +50,14 @@
                         </button>
 
             </div>
+    </form>
+    <form method="GET" action="{{ route('notebooks') }}">
+        @csrf
+        <div>
+                <button type="submit" class="btn btn-primary">
+                    {{ __('Show notebooks') }}
+                </button>
         </div>
     </form>
-        </body>
+</body>
 </html>
--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -1,11 +1,18 @@
 <head>
-    <title>List of notebooks</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css" rel="stylesheet">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Notebooks</title>
+    <<!-- CSS only -->
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<link href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.6.1.slim.js" integrity="sha256-tXm+sa1uzsbFnbXt8GJqsgi2Tw+m4BLGDof6eUPjbtk=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+<meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
 <div class="container">    
-    <table class="table table-bordered data-table">
+    <table id="notebooks-table" class="table table-bordered data-table">
         <thead>
             <tr>
                 <th>#</th>
@@ -45,5 +52,10 @@
     </table>
     <a href="/">Go Back</a>
 </div>
-      
+      <script>
+        $(document).ready(function () {
+            $('#notebooks-table').DataTable();
+            console.log(123);
+        });
+      </script>
 </body>
